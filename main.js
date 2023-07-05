204387a7c01801e831a90d03bc5de665cefbfdf9
applesandbananas123 <127574984+applesandbananas123@users.noreply.github.com>
2023-07-04 20:16:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a= first_name.concat(last_name);
+console.log(a);
+var a = 5;
+var b = 10;
+var c = b+a
+console.log(c);
+var d = b - a
+console.log(d)
